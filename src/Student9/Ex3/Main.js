a7d4c9b4f267c8fa1c34b225ecdfa94d917d4003
Melissa Schaefer <mschaefer@xpanxion.com>
2023-05-23 17:06:55 -0400

    update with Ex3

--- src/Student9/Ex3/Main.js
+++ src/Student9/Ex3/Main.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Main.css'
-
+import { UpDownCounter } from './UpDownCounter'
 export const Main = () => {
   return (
+    
     <div>
-      TODO...
+      
+      <UpDownCounter/>
+
     </div>
   )
 }
